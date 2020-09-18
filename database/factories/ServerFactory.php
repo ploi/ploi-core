20d15172d159a73e9edb54d1a71fcc441498ddea
Dennis Smink <dennissmink@gmail.com>
2020-09-18 09:07:53 +0200

    Fix factories and InertiaJS update

--- database/factories/ServerFactory.php
+++ database/factories/ServerFactory.php
@@ -1,25 +1,29 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\Models\Server;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+class ServerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Server::class;
 
-$factory->define(Server::class, function (Faker $faker) {
-    return [
-        'name' => $faker->slug(2),
-        'ip' => $faker->ipv4,
-        'internal_ip' => '127.0.0.1',
-    ];
-});
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
