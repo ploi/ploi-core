79bb522dee79d238aaececdbc7f389791b7693c8
Dennis <dennissmink@gmail.com>
2020-12-02 10:58:39 +0100

    Don’t allow horizon in production

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -34,7 +34,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewHorizon', function ($user) {
-            return $user->isAdmin();
+            return $user->isAdmin() && !config('app.demo');
         });
     }
 }
