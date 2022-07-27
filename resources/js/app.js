f2441990cd0f4cc001e09de033ab9e8f5a62b624
Dennis <dennissmink@gmail.com>
2022-07-27 13:39:11 +0200

    w

--- resources/js/app.js
+++ resources/js/app.js
@@ -40,35 +40,6 @@ const app = document.getElementById('app')
 
 let pageData = JSON.parse(app.dataset.page)
 
-// createInertiaApp({
-//     title: (title) => `${title} - ${appName}`,
-//     resolve: (name) => require(`./Pages/${name}.vue`),
-    // resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    // setup({el, App, props, plugin}) {
-    //     Vue
-    //         .use(plugin)
-            // .use(vClickOutside)
-            // .use(PortalVue)
-            // .use(VueMeta)
-            // .use(VueClipboard)
-            // .mixin(mixins)
-            // .component('InertiaLink', InertiaLink)
-            // .mixin({methods: {route}});
-
-        // new Vue({
-        //     render: h => h(App, props),
-        // })
-        //     .$mount(el);
-    // },
-// });
-
-// InertiaProgress.init({
-//     delay: 250,
-//     color: '#1b8ae8',
-//     includeCSS: true,
-//     showSpinner: false,
-// })
-
 new Vue({
     store,
     metaInfo: {
@@ -80,7 +51,6 @@ new Vue({
             resolveComponent: async(name) => {
                 const pages = import.meta.glob('./**/*.vue');
 
-                console.log(name);
                 return (await resolvePageComponent(`./Pages/${name}.vue`, pages)).default;
             }
         },
