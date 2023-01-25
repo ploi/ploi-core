a8361f7b22f753ab474fb7081e19de41eb96057a
Dennis <dennissmink@gmail.com>
2023-01-25 13:21:26 +0100

    force

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -4,8 +4,19 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $model = $this->getModel();
+        $record = new $model;
+        $record->forceFill($data);
+        $record->save();
+
+        return $record;
+    }
 }
