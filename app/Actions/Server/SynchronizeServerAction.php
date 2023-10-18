b4d35adfb4356eab07fbe8731737d92c04a1c5d0
Dennis <dennissmink@gmail.com>
2023-10-18 08:19:48 +0200

    wip

--- app/Actions/Server/SynchronizeServerAction.php
+++ app/Actions/Server/SynchronizeServerAction.php
@@ -22,6 +22,16 @@ class SynchronizeServerAction
             return null;
         }
 
+        if(!$serverData){
+            Notification::make()
+                ->title('Server synchronization')
+                ->body('It was not possible to synchronize servers, it seems the API key has the wrong scopes. Please make sure the Ploi API key you\'ve entered has all the scopes enabled.')
+                ->danger()
+                ->send();
+
+            return null;
+        }
+
         try {
             $server = Server::query()
                 ->updateOrCreate([
