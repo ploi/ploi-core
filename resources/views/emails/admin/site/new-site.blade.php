6556cf017a8d42ae4591f4a99823ba2d00d98249
Dennis <dennissmink@gmail.com>
2023-11-22 12:50:30 +0100

    wip

--- resources/views/emails/admin/site/new-site.blade.php
+++ resources/views/emails/admin/site/new-site.blade.php
@@ -3,7 +3,7 @@
 
 {{ __('A new site with the name **:site** has been created by the user :user.', ['site' => $site->domain, 'user' => $user->name]) }}
 
-{{ __('This site has been created on the server **:server**, the server currently has **:sites** of **:max** left.', ['server' => $server->name, 'sites' => $server->sites()->count(), 'max' => $server->maximum_sites]) }}
+{{ __('This site has been created on the server **:server**, the server currently has **:sites** of **:max** left.', ['server' => $server->name, 'sites' => $server->sites()->count(), 'max' => ($server->maximum_sites > 0 ? $server->maximum_sites : 'Unlimited')]) }}
 
 @component('mail::button', ['url' => \App\Filament\Resources\UserResource::getUrl('edit', ['record' => $user])])
 {{ __('View user') }}
