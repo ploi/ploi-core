c69f0b95b2e9496eb34036ab1aed3d00f7a051dc
Stan Menten <stanmenten@live.nl>
2024-12-19 20:51:01 +0100

    Redirect cleanup

--- app/Actions/Redirect/CreateRedirectAction.php
+++ app/Actions/Redirect/CreateRedirectAction.php
@@ -3,26 +3,21 @@
 namespace App\Actions\Redirect;
 
 use App\DataTransferObjects\RedirectData;
-use App\DataTransferObjects\SiteData;
 use App\Jobs\Redirects\CreateRedirect;
 use App\Models\Redirect;
 use App\Models\Server;
-use App\Models\Site;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 
 class CreateRedirectAction
 {
     public function execute(RedirectData $redirectData): ?Redirect
     {
-        // Determine the server
         $server = $this->determineServer($redirectData);
 
         if (!$server) {
             return null;
         }
 
-        $site = $server->sites()->find($redirectData->site_id); // Find a specific site related to the server
+        $site = $server->sites()->find($redirectData->site_id);
         if ($site) {
             $redirect = $site->redirects()->create($redirectData->toArray());
         } else {
@@ -31,7 +26,6 @@ class CreateRedirectAction
 
         dispatch(new CreateRedirect($redirect));
 
-        // Log the creation event in system logs
         $redirectData->getUser()->systemLogs()->create([
             'title' => __('New redirect created'),
             'description' => __('A new redirect has been created.'),
